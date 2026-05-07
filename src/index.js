3c949f3b4ecea3ed10d025c2503d1a10c4eac205
Anna Vernadskaya <anna-vernadskaya@yandex.ru>
2026-05-08 00:42:37 +0500

    feat: implement GuessingGame class

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.min = 0;
+    this.max = 0;
+    this.currentGuess = 0;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-  lower() { }
+  guess() {
+    this.currentGuess = Math.round((this.min + this.max) / 2);
 
-  greater() { }
-*/
+    return this.currentGuess;
+  }
+
+  lower() {
+    this.max = this.currentGuess;
+  }
+
+  greater() {
+    this.min = this.currentGuess;
+  }
 }
 
 module.exports = GuessingGame;
